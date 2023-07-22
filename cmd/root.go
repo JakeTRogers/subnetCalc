2895e0d875c027b48e7e32874cb809e8ada5b5fb
Jake Rogers <code@supportoss.org>
2023-07-22 14:53:40 -0600

    cmd/root - complete refactor using struct methods and added feature to enumerate subnets

--- cmd/root.go
+++ cmd/root.go
@@ -11,10 +11,14 @@ import (
 	"os"
 
 	"github.com/JakeTRogers/subnetCalc/utils"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 // flipBytes performs a bitwise XOR on each byte in the slice.
+// returns a slice of bytes with the bits flipped.
 func flipBytes(b []byte) []byte {
 	for i := 0; i < len(b); i++ {
 		b[i] ^= 0xFF
@@ -22,14 +26,61 @@ func flipBytes(b []byte) []byte {
 	return b
 }
 
-// getSubnetMask returns the subnet mask given the number of mask bits and the mask size.
-func getSubnetMask(maskBits, maskSize int) netip.Addr {
-	var maskBytes = make([]byte, maskSize/8)
+type network struct {
+	CIDR          netip.Prefix `json:"cidr"`
+	FirstHostIP   netip.Addr   `json:"firstIP"`
+	LastHostIP    netip.Addr   `json:"lastIP"`
+	NetworkAddr   netip.Addr   `json:"networkAddr"`
+	BroadcastAddr netip.Addr   `json:"broadcastAddr"`
+	SubnetMask    netip.Addr   `json:"subnetMask"`
+	MaskBits      int          `json:"maskBits"`
+	SubnetBits    int          `json:"subnetBits"`
+	MaxSubnets    uint         `json:"maxSubnets"`
+	MaxHosts      uint         `json:"maxHosts"`
+	MaskSize      int          `json:"-"`
+	Subnets       []network    `json:"subnets,omitempty"`
+}
+
+// getBroadcastAddr calculates the broadcast address for a subnet by ORing the network address and the inverted subnet mask.
+// returns the broadcast address as a netip.Addr.
+func (n network) getBroadcastAddr() netip.Addr {
+	invertedMask := flipBytes(n.SubnetMask.AsSlice())
+	var lastIPBytes = make([]byte, len(n.NetworkAddr.AsSlice()))
+
+	for i := 0; i < len(n.NetworkAddr.AsSlice()); i++ {
+		lastIPBytes[i] = n.NetworkAddr.AsSlice()[i] | invertedMask[i]
+	}
+	b, _ := netip.AddrFromSlice(lastIPBytes)
+	return b
+}
+
+// getSubnetBits calculates the available subnet bits for a given network address and mask bits based on the network class.
+// returns an integer representing the number of subnet bits.
+func (n network) getSubnetBits() int {
+	firstOctet := n.NetworkAddr.AsSlice()[0]
+	switch {
+	case firstOctet < 128:
+		return n.MaskBits - 8
+	case firstOctet < 192:
+		return n.MaskBits - 16
+	case firstOctet < 224:
+		return n.MaskBits - 24
+	case firstOctet < 240:
+		return n.MaskBits - 32
+	default:
+		return n.MaskBits - 40
+	}
+}
+
+// getSubnetMask calculates the subnet mask given the number of mask bits and the mask size.
+// returns the subnet mask as a netip.Addr.
+func (n network) getSubnetMask() netip.Addr {
+	var maskBytes = make([]byte, n.MaskSize/8)
 	for i := 0; i < len(maskBytes); i++ {
 		for j := 0; j < 8; j++ {
-			if maskBits > 0 {
+			if n.MaskBits > 0 {
 				maskBytes[i] |= 1 << uint(7-j)
-				maskBits--
+				n.MaskBits--
 			}
 		}
 	}
@@ -37,82 +88,119 @@ func getSubnetMask(maskBits, maskSize int) netip.Addr {
 	return subnetMask
 }
 
-// getSubnetBits returns the available subnet bits for a given network address and mask bits.
-func getSubnetBits(networkAddr []byte, maskBits int) int {
-	firstOctet := networkAddr[0]
-	if firstOctet < 128 {
-		return maskBits - 8
-	} else if firstOctet < 192 {
-		return maskBits - 16
-	} else if firstOctet < 224 {
-		return maskBits - 24
-	} else if firstOctet < 240 {
-		return maskBits - 32
-	} else {
-		return maskBits - 40
+// getSubnets calculates the number of subnets that will fit in a supernet using the provided subnet mask bits.
+// returns a slice of network structs contained in a supernet.
+func (n *network) getSubnets(subnetMaskBits int) {
+	// get the number of subnets of size 'subnetMaskBits' that will fit in the supernet
+	numSubnets := int(math.Pow(2, float64(subnetMaskBits-n.MaskBits)))
+
+	for i := 0; i < numSubnets; i++ {
+		if i == 0 {
+			n.Subnets = append(n.Subnets, getNetworkDetails(fmt.Sprintf("%s/%d", n.NetworkAddr, subnetMaskBits)))
+		} else {
+			n.Subnets = append(n.Subnets, getNetworkDetails(fmt.Sprintf("%s/%d", n.Subnets[i-1].BroadcastAddr.Next(), subnetMaskBits)))
+		}
 	}
 }
 
-// lastIP returns the last IP address in a subnet given an IP address and a subnet mask.
-func lastIP(ip []byte, mask []byte) netip.Addr {
-	invertedMask := flipBytes(mask)
-	var lastBytes = make([]byte, len(ip))
-	for i := 0; i < len(ip); i++ {
-		lastBytes[i] = ip[i] | invertedMask[i]
-	}
-	lastIP, _ := netip.AddrFromSlice(lastBytes)
-	return lastIP
-}
+// printNetwork prints information about an IP network to stdout.
+func (n network) printNetwork() {
+	// Use the message package to format large numbers with commas
+	p := message.NewPrinter(language.English)
 
-// printNetworkInformation will print a network struct in a human readable format.
-func printNetworkInformation(n network) {
-	fmt.Printf("\n            IP Address: %s\n", n.IpAddr)
-	fmt.Printf("           Subnet Mask: %s\n\n", n.SubnetMask)
+	fmt.Println()
+	fmt.Println("               Network:", n.CIDR)
 	fmt.Println("    Host Address Range:", n.FirstHostIP, "-", n.LastHostIP)
-	fmt.Println("       Network Address:", n.NetworkAddr)
 	fmt.Println("     Broadcast Address:", n.BroadcastAddr)
-	fmt.Println("       Maximum Subnets:", n.MaxSubnets)
-	fmt.Println("      Hosts Per Subnet:", n.HostsPerSubnet)
+	fmt.Println("           Subnet Mask:", n.SubnetMask)
+	p.Println("       Maximum Subnets:", n.MaxSubnets)
+	p.Println("         Maximum Hosts:", n.MaxHosts)
 }
 
-type network struct {
-	BroadcastAddr  netip.Addr   `json:"broadcast_addr"`
-	CIDR           netip.Prefix `json:"cidr"`
-	FirstHostIP    netip.Addr   `json:"first_ip"`
-	HostsPerSubnet int          `json:"hosts_per_subnet"`
-	IpAddr         netip.Addr   `json:"ip_addr"`
-	LastHostIP     netip.Addr   `json:"last_ip"`
-	MaskBits       int          `json:"mask_bits"`
-	MaskSize       int          `json:"mask_size"`
-	MaxSubnets     int          `json:"max_subnets"`
-	NetworkAddr    netip.Addr   `json:"network_addr"`
-	SubnetBits     int          `json:"subnet_bits"`
-	SubnetMask     netip.Addr   `json:"subnet_mask"`
+// printJSON will print a network struct in json format.
+func (n network) printNetworkJSON() {
+	netJSON, err := json.MarshalIndent(n, "", "  ")
+	if err != nil {
+		utils.Log.Fatal().Msg(err.Error())
+	}
+	fmt.Println(string(netJSON))
 }
+
+// printSubnets uses the table package to print subnet information in a table.
+func (n network) printSubnets(color bool) {
+	p := message.NewPrinter(language.English)
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	if color {
+		t.SetStyle(table.StyleColoredBlackOnBlueWhite)
+	} else {
+		t.SetStyle(table.StyleRounded)
+	}
+	t.AppendHeader(table.Row{"#", "SUBNET", "FIRST IP", "LAST IP", "BROADCAST", "HOSTS"})
+
+	for i, s := range n.Subnets {
+		t.AppendRow([]interface{}{i + 1, s.CIDR, s.FirstHostIP, s.LastHostIP, s.BroadcastAddr, p.Sprint(s.MaxHosts)})
+	}
+
+	fmt.Printf("\n  %v contains %d /%d subnets:\n", n.CIDR, len(n.Subnets), n.Subnets[0].MaskBits)
+	t.Render()
+}
+
+// getNetworkDetails takes a CIDR and returns a network struct with details about the network
+// returns a network struct containing network details.
+func getNetworkDetails(cidr string) network {
+	var n network
+	var err error
+
+	// use netip package to confirm the provided input is a valid ipv4 or ipv6 CIDR
+	inputCIDR, err := netip.ParsePrefix(cidr)
+	if err != nil {
+		utils.Log.Fatal().Msg(err.Error())
+	}
+
+	n.CIDR = netip.MustParsePrefix(fmt.Sprintf("%s/%d", inputCIDR.Masked().Addr(), inputCIDR.Bits()))
+	n.NetworkAddr = n.CIDR.Masked().Addr()
+	n.MaskBits = n.CIDR.Bits()
+	n.MaskSize = n.CIDR.Addr().BitLen()
+	n.SubnetMask = n.getSubnetMask()
+	n.BroadcastAddr = n.getBroadcastAddr()
+	n.FirstHostIP = n.NetworkAddr.Next()
+	n.LastHostIP = n.BroadcastAddr.Prev()
+	n.SubnetBits = n.getSubnetBits()
+	n.MaxSubnets = uint(math.Pow(2, float64(n.SubnetBits)))
+	n.MaxHosts = 1<<(n.MaskSize-n.MaskBits) - 2
+	return n
+}
+
+var color bool
+var subnetMaskBits int
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "subnetCalc",
-	Version: "v0.0.1",
+	Use:     "subnetCalc <CIDR>",
+	Version: "v0.1.0",
 	Short:   "calculate subnet",
 	Long: `subnetCalc is a CLI application to calculate subnets when given an IP address and a subnet mask in CIDR notation. It
-will return the requested IP, subnet mask, host address range, network address, broadcast address, subnet bits, mask
-bits, mask size, maximum number of subnets, max hosts per subnet.
+will return the requested network, host address range, broadcast address, subnet mask, maximum number of subnets, and
+the maximum number hosts.
 
-	example:
-	    > subnetCalc 10.12.34.56/19
+subnetCalc can also be used to carve up a network into subnets by providing subnet mask size. It then lists them in a
+either table or JSON format.
 
-                    IP Address: 10.12.34.56
-                   Subnet Mask: 255.255.224.0
+Examples:
+  # Get network information for a CIDR:
+  subnetCalc 10.12.34.56/19
 
-            Host Address Range: 10.12.32.1 - 10.12.63.254
-               Network Address: 10.12.32.0
-             Broadcast Address: 10.12.63.255
-               Maximum Subnets: 2048
-              Hosts Per Subnet: 8190`,
+  # Get network information for a CIDR and carve it up into subnets:
+  subnetCalc 10.12.0.0/16 --subnet_size 18
+
+  # Get network information for a CIDR, carve it up into subnets, and print the output in JSON format:
+  subnetCalc 192.168.10.0/24 --subnet_size 26 --json
+`,
 
 	PersistentPreRun: utils.SetLogLevel,
 	Run: func(cmd *cobra.Command, args []string) {
+		// if no arguments are provided, print help
 		if len(args) == 0 {
 			if err := cmd.Help(); err != nil {
 				utils.Log.Fatal().Msg(err.Error())
@@ -122,67 +210,31 @@ bits, mask size, maximum number of subnets, max hosts per subnet.
 			utils.Log.Fatal().Msg("too many arguments, expected CIDR notation")
 		}
 
-		var (
-			n   network
-			err error
-		)
+		// populate network struct with details of the provided CIDR
+		n := getNetworkDetails(args[0])
 
-		// use netip package to confirm the provided input is a valid ipv4 or ipv6 CIDR
-		inputCIDR, err := netip.ParsePrefix(args[0])
-		if err != nil {
-			utils.Log.Fatal().Msg(err.Error())
-		}
-		utils.Log.Debug().Msgf("input cidr: %s", inputCIDR)
-
-		// use netip package to extract IP and mask information from user provided CIDR
-		n.CIDR = netip.MustParsePrefix(fmt.Sprintf("%s/%d", inputCIDR.Masked().Addr(), inputCIDR.Bits()))
-		n.IpAddr = inputCIDR.Addr()
-		n.MaskBits = n.CIDR.Bits()
-		n.MaskSize = n.IpAddr.BitLen()
-		utils.Log.Debug().Msgf("ip: %s, mask bits: %d, mask size: %d", n.IpAddr, n.MaskBits, n.MaskSize)
-
-		// calculate subnet mask from mask bits
-		n.SubnetMask = getSubnetMask(n.MaskBits, n.MaskSize)
-		utils.Log.Debug().Msgf("subnet mask: %s", n.SubnetMask)
-
-		// get network address using IP and mask
-		n.NetworkAddr = n.CIDR.Masked().Addr()
-
-		// calculate the broadcast address using the network address and mask
-		n.BroadcastAddr = lastIP(n.NetworkAddr.AsSlice(), n.SubnetMask.AsSlice())
-		utils.Log.Debug().Msgf("Network Address: %s, Broadcast Address: %s", n.NetworkAddr, n.BroadcastAddr)
-
-		// calculate the host address range using the network address and broadcast address
-		n.FirstHostIP = n.NetworkAddr.Next()
-		n.LastHostIP = n.BroadcastAddr.Prev()
-		utils.Log.Debug().Msgf("ip range: %s - %s", n.FirstHostIP, n.LastHostIP)
-
-		// calculate available subnet bits based on network address and mask bits
-		n.SubnetBits = getSubnetBits(n.NetworkAddr.AsSlice(), n.MaskBits)
-		utils.Log.Debug().Msgf("subnet bits %d", n.SubnetBits)
-
-		// calculate maximum number of subnets
-		n.MaxSubnets = int(math.Pow(2, float64(n.SubnetBits)))
-		utils.Log.Debug().Msgf("max subnets: %d", n.MaxSubnets)
-
-		// calculate hosts per subnet
-		n.HostsPerSubnet = 1<<(n.MaskSize-n.MaskBits) - 2
-		utils.Log.Debug().Msgf("hosts per subnet: %d", n.HostsPerSubnet)
-
-		if cmd.Flags().Changed("json") {
-			netJSON, err := json.MarshalIndent(n, "", "  ")
-			if err != nil {
-				utils.Log.Fatal().Msg(err.Error())
+		// if subnet_size flag is set, carve up the supernet into subnets of the requested size
+		if cmd.Flags().Changed("subnet_size") {
+			// check if subnet mask bits are larger than the supernet's mask bits
+			if subnetMaskBits <= n.MaskBits {
+				utils.Log.Fatal().Msgf("subnet mask bits, %d, must be larger than the supernet's mask bits: %d", subnetMaskBits, n.MaskBits)
 			}
-			fmt.Print(string(netJSON))
+			// populate n.subnets with a slice of network structs containing subnet details
+			n.getSubnets(subnetMaskBits)
+		}
+
+		// print the network details in the requested format
+		if cmd.Flags().Changed("json") {
+			n.printNetworkJSON()
 		} else {
-			printNetworkInformation(n)
+			n.printNetwork()
+			if n.Subnets != nil {
+				n.printSubnets(color)
+			}
 		}
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -192,6 +244,9 @@ func Execute() {
 
 func init() {
 	rootCmd.SetVersionTemplate("subnetCalc {{.Version}}\n")
+	rootCmd.Flags().BoolVarP(&color, "color", "c", false, "output subnet table in color")
 	rootCmd.Flags().BoolP("json", "j", false, "output information for the requested CIDR in json format")
+	rootCmd.MarkFlagsMutuallyExclusive("color", "json")
+	rootCmd.Flags().IntVarP(&subnetMaskBits, "subnet_size", "s", 0, "number of subnet mask bits to be used in carving up the supernet")
 	rootCmd.PersistentFlags().CountP("verbose", "v", "increase verbosity")
 }
