576700b2b77f5023f1af6616932a2edf8a2a3baf
Jake Rogers <code@supportoss.org>
2026-01-02 17:34:12 -0700

    bump: version 1.0.1 → 1.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 	"github.com/JakeTRogers/subnetCalc/tui"
 )
 
-var version = "v1.0.1"
+var version = "v1.1.0"
 
 // rootCmd is the package-level command instance used by Execute().
 // Tests should use NewRootCommand() for isolated instances.
